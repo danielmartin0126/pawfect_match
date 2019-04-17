9921ae3d08aeb4cb4a050880f74ef8ad9bf1148c
ezevin <ehzevin@gmail.com>
2019-04-17 11:43:18 -0400

    login page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # resources :logins
   resources :messages
 
-
+  root  "sessions#new"
   get "/login", to: "sessions#new", as: 'login'
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: 'logout'
